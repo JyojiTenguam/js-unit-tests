06d6a2a2ffe76ec55dd18c5a097f30a13e99245f
JyojiTenguam <j.y.oji@hotmail.com>
2024-01-24 00:37:57 -0300

    Correção requisito 07 de Project js unit tests

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -33,15 +33,12 @@
   arrayGenerator('values', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ 3, 2, 0, -1 ]
   arrayGenerator('entries', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 0 ], [ 'sub', -1 ] ]
 */
-
-const calculator = (number1, number2) => { 
-return {
+const calculator = (number1, number2) => ({
   sum: number1 + number2,
   mult: number1 * number2,
   div: Math.floor(number1 / number2),
   sub: number1 - number2,
-};
-};
+});
 
 const arrayGenerator = (type, object) => {
   if (type === 'keys') {
